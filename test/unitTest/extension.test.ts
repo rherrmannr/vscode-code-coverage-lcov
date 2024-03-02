4a98664c9164e832492a1e691fd4048bb01a2fe5
Robin Herrmann <Robin.Herrmann@dormakaba-external.com>
2024-03-02 22:05:22 +0100

    add coverage

--- test/unitTest/extension.test.ts
+++ test/unitTest/extension.test.ts
@@ -3,7 +3,7 @@ import * as assert from "assert";
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from "vscode";
-import { getConfig } from "../config";
+import { getConfig } from "../../src/config";
 // import * as myExtension from '../../extension';
 
 suite("Extension Test Suite", () => {
