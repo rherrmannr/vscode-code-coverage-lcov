4ca051a1202a28d6247bbc692b20e728e606a088
Robin Herrmann <Robin.Herrmann@dormakaba-external.com>
2024-03-08 12:51:31 +0100

    cleaned up config tests

--- test/unitTest/config.test.ts
+++ test/unitTest/config.test.ts
@@ -1,15 +1,10 @@
 import * as assert from "assert";
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from "vscode";
 import { getConfig } from "../../src/config";
 import sinon from "sinon";
-// import * as myExtension from '../../extension';
 
 suite("Config Test Suite", () => {
-  vscode.window.showInformationMessage("Start all tests.");
-
   test("Default Values Test", () => {
     // given
     // when
